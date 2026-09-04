bd8c89554bde6e752642c440bc5222a1faa3c3f7
Ravsean <emailkamu@gmail.com>
2026-09-04 22:45:37 +0700

    week01: initial setup and procedural code

--- src/oop_167618_rizki_week01/GradingSystem.kt
+++ src/oop_167618_rizki_week01/GradingSystem.kt
@@ -1,10 +1,11 @@
-package OOP_167618_rizki_week01
+package oop_167618_rizki_week01
 
 fun main() {
     var name: String = "rizki"
     var score: Int = 80
 
     // Concatenation gaya lama
-    println(Nama: "+ name +", Nilai)
+    println("Nama: " + name + ", Nilai: " + score)
+
 }
 
