e492cf75655ec513136a7b062bdb66f4afa328ff
Ravsean <emailkamu@gmail.com>
2026-09-04 22:54:09 +0700

    week01: complete grading logic and add calculateStatus function

--- src/oop_167618_rizki_week01/GradingSystem.kt
+++ src/oop_167618_rizki_week01/GradingSystem.kt
@@ -1,22 +1,23 @@
 package oop_167618_rizki_week01
 
+// Fungsi Expression Body (Tulis di luar main)
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
+
 fun main() {
-    // REFACTOR: Gunakan val dan hapus tipe data eksplisit
     val name = "rizki"
     val score = 80
 
-    // REFACTOR: String Template ($name)
     println("Nama: $name, Nilai: $score")
 
+    // Logika When Expression yang benar dan bersih
     val grade = when (score) {
         in 90..100 -> "A"
         in 80..89 -> "B"
         in 70..79 -> "C"
-
+        else -> "D"
     }
     println("Grade kamu: $grade")
+
+    // Memanggil fungsi kalkulasi status
+    println("Status: ${calculateStatus(score)}")
 }
-
-
-
-
