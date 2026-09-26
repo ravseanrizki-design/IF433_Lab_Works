09ec2d8c8481663b9c96e126a9531adc6b9ec630
Mohamad Rizki Patriotik <ravsean.rizki@gmail.com>
2026-09-26 10:51:51 +0700

    week05: create Admin class inheriting Pegawai

--- src/oop_00000167618_MohamadRizkiPatriotik/week05/Dosen.kt
+++ src/oop_00000167618_MohamadRizkiPatriotik/week05/Dosen.kt
@@ -1,4 +1,14 @@
 package oop_00000167618_MohamadRizkiPatriotik.week05
 
-class Dosen {
+
+class Dosen(nama: String, val nidn: String) : Pegawai(nama) {
+    // WAJIB di-override karena fungsi bekerja() bersifat abstract di Parent
+    override fun bekerja() {
+        println("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS.")
+    }
+
+    // Fungsi unik/spesifik yang hanya dimiliki Dosen
+    fun mengajar() {
+        println("[$nama] sedang mengajar mahasiswa di kelas.")
+    }
 }
